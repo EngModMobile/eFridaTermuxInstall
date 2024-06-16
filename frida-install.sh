36795e2c8a86af73d8b67a3a2125df5313606bd3
Zunzz <155456585+Zunzz@users.noreply.github.com>
2024-06-16 12:10:51 -0300

    Update frida-install.sh

--- frida-install.sh
+++ frida-install.sh
@@ -8,20 +8,14 @@ if ! (dpkg -l | grep -q tar); then
    pkg install tar -y
 fi
 
-if ! (dpkg -l | grep -q jq); then
-   pkg install jq -y
-fi
-
-latest_version=$(curl -s https://api.github.com/repos/frida/frida/releases/latest | jq -r '.tag_name')
-
 
 if [[ $(getprop ro.product.cpu.abi) == "arm64"* ]]; then
-   curl -L  https://github.com/frida/frida/releases/download/$latest_version/frida-core-devkit-$latest_version-android-arm64.tar.xz > frida_core_devkit.tar.xz
+   curl -L  https://github.com/frida/frida/releases/download/16.2.0/frida-core-devkit-16.2.0-android-arm64.tar.xz > frida_core_devkit.tar.xz
 else
-   curl -L  https://github.com/frida/frida/releases/download/$latest_version/frida-core-devkit-$latest_version-android-arm.tar.xz > frida_core_devkit.tar.xz
+   curl -L  https://github.com/frida/frida/releases/download/16.2.0/frida-core-devkit-16.2.0-android-arm.tar.xz > frida_core_devkit.tar.xz
 fi
 
-   mkdir $HOME/frida-core
+mkdir $HOME/frida-core
 
 tar -xf frida_core_devkit.tar.xz -C ~/frida-core 
 
